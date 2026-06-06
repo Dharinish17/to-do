7667842da37736d6ffde7471f9457654fbb82361
Dharinish17 <dharinish2006@gmail.com>
2026-06-06 23:29:24 +0530

    made the counters and 'Mark as Complete' button work

--- js/script.js
+++ js/script.js
@@ -3,6 +3,9 @@ document.addEventListener("DOMContentLoaded", () => {
   let taskDate = document.getElementById("taskDate");
   let addTask = document.getElementById("addTask");
   let taskList = document.getElementById("taskList");
+  let total = document.getElementById("totalCount");
+  let done = document.getElementById("doneCount");
+  let pending = document.getElementById("pendingCount");
 
   let taskArr = JSON.parse(localStorage.getItem("user")) || [];
 
@@ -15,7 +18,7 @@ document.addEventListener("DOMContentLoaded", () => {
       name: "",
       id: "",
       date: "",
-      completed: false
+      completed: false,
     };
     obj.name = taskName.value;
     obj.date = taskDate.value;
@@ -25,20 +28,22 @@ document.addEventListener("DOMContentLoaded", () => {
     taskName.value = "";
     taskDate.value = "";
     display(); //displays the task when added by the add task btn
-});
-display(); //makes sure the display function keeps the taks in the web even when the web is re-loaded
+    chkCounters();
+  });
+  display(); //makes sure the display function keeps the taks in the web even when the web is re-loaded
+  chkCounters();
+  changeMarkComplete();
 
   function display() {
     // retrieving values from the local storage
-    taskList.innerHTML = ""
+    taskList.innerHTML = "";
     let val = JSON.parse(localStorage.getItem("user"));
-    if(val=== null)
-    {
-        return;
+    if (val === null) {
+      return;
     }
 
-    val.forEach(task => {
-        taskList.appendChild(createTaskCard(task));
+    val.forEach((task) => {
+      taskList.appendChild(createTaskCard(task));
     });
   }
 
@@ -53,31 +58,75 @@ display(); //makes sure the display function keeps the taks in the web even when
             </div>
             <div class="md:flex md:gap-4">
             <button
-              class="bg-[#22C55E] rounded-sm p-1 font-semibold block mt-4 md:mt-4 hover:bg-[#05ad42]"
+              class="complete-btn bg-[#22C55E] rounded-sm p-1 font-semibold block mt-4 md:mt-4 hover:bg-[#05ad42]"
+              id= "${task.id}"
             >
               Mark As Complete
             </button>
             <button
-              class="bg-[#EF4444] rounded-sm p-1 font-semibold block mt-2 md:mt-4 hover:bg-[#8f0707]"
+              class="delete-btn bg-[#EF4444] rounded-sm p-1 font-semibold block mt-2 md:mt-4 hover:bg-[#8f0707]"
+              id= "${task.id}"
             >
               Delete
             </button>
           </div>
         `;
 
-        taskCard.dataset.id= `${task.id}`;
+    taskCard.dataset.id = `${task.id}`;
 
     return taskCard;
   }
 
+  function chkCounters() {
+    let t = JSON.parse(localStorage.getItem("user")) || [];
+    let ttl = 0;
+    let dne = 0;
+    let pen = 0;
 
+    t.forEach((t) => {
+      ttl++;
+      if (t.completed) {
+        dne++;
+      } else {
+        pen++;
+      }
+    });
+    total.innerText = ttl;
+    done.innerText = dne;
+    pending.innerText = pen;
+  }
 
+  function changeMarkComplete() {
+    let t = JSON.parse(localStorage.getItem("user"));
+    t.forEach((task) => {
+      if (task.completed) {
+        let cid = task.id;
+        let des = document.getElementById(String(cid));
+        des.classList.remove(
+          "bg-[#22C55E]",
+          "rounded-sm",
+          "p-1",
+          "hover:bg-[#05ad42]",
+        );
+        des.classList.add("taskComplete");
+        des.innerText = "Task Completed";
+      }
+    });
+  }
 
+  taskList.addEventListener("click", (e) => {
+    if (e.target.classList.contains("complete-btn")) {
+      let completeId = e.target.id;
 
-
-
-
-
-
-
+      let tsk = JSON.parse(localStorage.getItem("user"));
+      tsk.forEach((task) => {
+        if (task.id == completeId) {
+          task.completed = true;
+        }
+      });
+      localStorage.setItem("user", JSON.stringify(tsk));
+      changeMarkComplete();
+    }
+    chkCounters();
+  });
 });
